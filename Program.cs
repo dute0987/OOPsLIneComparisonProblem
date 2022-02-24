665fc1e29a2b3f812fcc11db3f80eed61be5e28e
dute0987 <dute0987@gmail.com>
2022-02-24 17:53:14 +0530

    Program.cs for Check equality of two line

--- Program.cs
+++ Program.cs
@@ -8,13 +8,13 @@ namespace Day_8_LineComparisonUsingOOps
             //Welcome Message for Line Comparison Using OOPs Concept
             //Console.WriteLine("Welcome to the Program of Comparison of Line");
 
-            //UC-1-Problem
-            UC_1_LengthCalculation UC1 = new UC_1_LengthCalculation();
-            UC1.CalculationOfLength();
+            ////UC-1-Problem
+            //UC_1_LengthCalculation UC1 = new UC_1_LengthCalculation();
+            //UC1.CalculationOfLength();
 
-            ////UC-2-Problem
-            //UC_2_EqualityOfTwoLine UC2 = new UC_2_EqualityOfTwoLine();
-            //UC2.TwoLineEquality();
+            //UC-2-Problem
+            UC_2_EqualityOfTwoLine UC2 = new UC_2_EqualityOfTwoLine();
+            UC2.TwoLineEquality();
 
             ////UC-3-Problem
             //UC_3_LineEqualGreaterSamller UC3 = new UC_3_LineEqualGreaterSamller();
