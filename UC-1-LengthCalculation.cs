18246c0b5a79f65075c68d840f5799be1a81c39e
dute0987 <dute0987@gmail.com>
2022-02-24 17:51:31 +0530

    Main file for Length calculation

---
+++ UC-1-LengthCalculation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_LineComparisonUsingOOps
+{
+    public class UC_1_LengthCalculation
+    {
+     public void CalculationOfLength()
+        {
+            double x1, x2, y1, y2;
+            double LengthOfLine;
+
+            Console.WriteLine("Please Enter The Value for x1 : ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter The Value for x2 : ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter The Value for y1 : ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter The Value for y2 : ");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            LengthOfLine = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine("the length of Line is :- " + LengthOfLine);
+        }
+    }
+}
