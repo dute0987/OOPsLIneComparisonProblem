0ede6335280787c5e11f1284fedf72685ebcfbbb
dute0987 <dute0987@gmail.com>
2022-02-24 17:49:34 +0530

    Program.cs for length calculation

--- Program.cs
+++ Program.cs
@@ -6,11 +6,11 @@ namespace Day_8_LineComparisonUsingOOps
         public static void Main(string[] args)
         {
             //Welcome Message for Line Comparison Using OOPs Concept
-             Console.WriteLine("Welcome to the Program of Comparison of Line");
+            //Console.WriteLine("Welcome to the Program of Comparison of Line");
 
-            ////UC-1-Problem
-            //UC_1_LengthCalculation UC1 = new UC_1_LengthCalculation();
-            //UC1.CalculationOfLength();
+            //UC-1-Problem
+            UC_1_LengthCalculation UC1 = new UC_1_LengthCalculation();
+            UC1.CalculationOfLength();
 
             ////UC-2-Problem
             //UC_2_EqualityOfTwoLine UC2 = new UC_2_EqualityOfTwoLine();
@@ -19,7 +19,7 @@ namespace Day_8_LineComparisonUsingOOps
             ////UC-3-Problem
             //UC_3_LineEqualGreaterSamller UC3 = new UC_3_LineEqualGreaterSamller();
             //UC3.CheckLineEqualGreaterLess();
-            
+
         }
     }
 }
